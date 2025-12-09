f55b9ae4006f37ee337fb4013a4045083b7bd625
hector <hectorda28yo@gmail.com>
2025-12-09 11:24:18 -0500

    Habilitacion de CORS para Vercel

--- src/main/java/com/educatoon/backend/security/config/SecurityConfig.java
+++ src/main/java/com/educatoon/backend/security/config/SecurityConfig.java
@@ -79,7 +79,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:4200",
+            "https://frontend-webservice.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
             "Authorization", 
